2b6f146fedb8d9b5506f22ed9b94f8ce7c744c75
Melvin Quick <melvinquick@proton.me>
2025-01-03 15:53:46 -0500

    Initial commit of class being built. Can currently generate passwords with lowercase letters, uppercase letters, numbers, and symbols.

---
+++ psswd_box/password_generator.py
@@ -0,0 +1,157 @@
+from secrets import choice
+
+
+class PasswordGenerator:
+    def __init__(self, character_types=["y", "y", "y", "y"], num_characters=20):
+        self.character_types = character_types
+        self.num_characters = num_characters
+        self.lowercase_letters = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "x",
+            "x",
+            "y",
+            "z",
+        ]
+        self.uppercase_letters = [
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+        ]
+        self.numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.symbols = [
+            "!",
+            "@",
+            "#",
+            "$",
+            "%",
+            "^",
+            "&",
+            "*",
+            "(",
+            ")",
+            "[",
+            "]",
+            "{",
+            "}",
+            "\\",
+            ";",
+            ":",
+            '"',
+            "'",
+            "<",
+            ">",
+            "/",
+            "?",
+            ",",
+            "`",
+            "~",
+        ]
+
+    def generate_password(self):
+        if self.character_types == ["n", "n", "n", "n"]:
+            print("You didn't include any character types... Exiting")
+            return exit()
+        choices = self.get_valid_choices()
+        counter = 0
+        password = ""
+
+        while counter < self.num_characters:
+            password += choice(choices)
+            counter += 1
+
+        return password
+
+    def get_valid_choices(self):
+        match self.character_types:
+            case ["y", "y", "y", "y"]:
+                return (
+                    self.lowercase_letters
+                    + self.uppercase_letters
+                    + self.numbers
+                    + self.symbols
+                )
+            case ["y", "y", "y", "n"]:
+                return self.lowercase_letters + self.uppercase_letters + self.numbers
+            case ["y", "y", "n", "n"]:
+                return self.lowercase_letters + self.uppercase_letters
+            case ["y", "n", "n", "n"]:
+                return self.lowercase_letters
+            case ["y", "n", "y", "y"]:
+                return self.lowercase_letters + self.numbers + self.symbols
+            case ["y", "n", "y", "n"]:
+                return self.lowercase_letters + self.numbers
+            case ["y", "n", "n", "y"]:
+                return self.lowercase_letters + self.symbols
+            case ["y", "y", "n", "y"]:
+                return self.lowercase_letters + self.uppercase_letters + self.symbols
+            case ["n", "y", "y", "y"]:
+                return self.uppercase_letters + self.numbers + self.symbols
+            case ["n", "n", "y", "y"]:
+                return self.numbers + self.symbols
+            case ["n", "n", "n", "y"]:
+                return self.symbols
+            case ["n", "y", "n", "y"]:
+                return self.uppercase_letters + self.symbols
+            case ["n", "y", "n", "n"]:
+                return self.uppercase_letters
+            case ["n", "n", "y", "n"]:
+                return self.numbers
+            case ["n", "y", "y", "n"]:
+                return self.uppercase_letters + self.numbers
+            case ["n", "n", "n", "n"]:
+                print("You didn't include any character types... Exiting")
+                return exit()
+
+
+def main():
+
+    password = PasswordGenerator()
+    print(password.generate_password())
+
+
+if __name__ == "__main__":
+    main()
