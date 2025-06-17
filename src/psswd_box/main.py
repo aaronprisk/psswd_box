7d3378ceb788437a54c94b1295c3852733ec2de6
Aaron J Prisk <aaronprisk@gmail.com>
2025-06-17 16:39:26 -0400

    Update main.py

--- src/psswd_box/main.py
+++ src/psswd_box/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import sys
+import os
+print("QT_PLUGIN_PATH:", os.environ.get("QT_PLUGIN_PATH"))
+print("LD_LIBRARY_PATH:", os.environ.get("LD_LIBRARY_PATH"))
+
 sys.path.insert(0, "/snap/psswd-box/current/lib")
 
 from psswd_box.app import main
