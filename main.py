3efacdc0949be4f471417384847953c4277521e7
Aaron J Prisk <aaronprisk@gmail.com>
2025-06-17 17:29:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import os
+os.environ.setdefault("QT_QPA_PLATFORM", "xcb")  # fallback if Wayland fails
+
 from psswd_box.app import main
 
 if __name__ == "__main__":
     main()
-
