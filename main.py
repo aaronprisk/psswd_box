664d45e9e7c82e55312b4510337933b5bbd34f8c
Aaron J Prisk <aaronprisk@gmail.com>
2025-06-17 17:37:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import os
-os.environ.setdefault("QT_QPA_PLATFORM", "xcb")  # fallback if Wayland fails
+import sys
+
+# Ensure Python can find the bundled app code
+sys.path.insert(0, os.path.join(os.environ["SNAP"], "lib"))
+
+os.environ.setdefault("QT_QPA_PLATFORM", "xcb")  # fallback to X11 if Wayland fails
 
 from psswd_box.app import main
 
